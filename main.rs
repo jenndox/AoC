80086a5e552910537943c90d532b89b2cc8c1a76
Jenn Lin <doxtader@gmail.com>
2020-12-02 22:54:07 -0800

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,27 @@
+mod helper;
+mod day1alt;
+mod day2;
+        
+pub use crate::day1alt::report;
+pub use crate::day2::password;
+pub use crate::helper::helper_fns;
+    
+use std::env;
+            
+fn main() { 
+    let args: Vec<String> = env::args().collect();
+            
+    let day = &args[1];
+    let filename = &args[2];
+
+    println!("This is Day {} of advent", day);
+    println!("Input file {}", filename);
+            
+    match day.parse().unwrap_or(1) {
+        1 => report::report(filename, 3),
+        201 => password::passwords_first(filename),
+        2 => password::passwords(filename),
+        // Handle the rest of cases
+        _ => println!("Not an Advent Day we have done yet."),
+    }
+}
