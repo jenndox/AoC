7ac6a99d53d683f876ed53ba14d86b3fa241e374
Jenn Lin <doxtader@gmail.com>
2020-12-02 22:53:50 -0800

    Create helper.rs

---
+++ helper.rs
@@ -0,0 +1,24 @@
+pub mod helper_fns {
+    use std::{
+        fs::File,
+        io::{prelude::*, BufReader},
+    };
+
+    pub fn file_into_vec(filename: &String) -> Vec<String> {
+        let contents = File::open(filename)
+            .expect("Something went wrong reading the file");
+        let buf = BufReader::new(contents);
+        buf.lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect()
+    }
+
+    pub fn file_into_int_vec(filename: &String) -> Vec<i64> {
+        let contents = File::open(filename)
+            .expect("Something went wrong reading the file");
+        let buf = BufReader::new(contents);
+        buf.lines()
+            .map(|l| l.expect("Could not parse line").parse().unwrap_or(0))
+            .collect()
+    }
+}
