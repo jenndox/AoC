c66825159037134b143b68efea17a63cf2d10c2e
Jenn Lin <jennlin@amazon.com>
2020-12-11 12:43:05 -0800

    Add day 10

--- cargo-project/src/day10.rs
+++ cargo-project/src/day10.rs
@@ -1,48 +1,52 @@
 pub mod jolt {
+    use std::collections::HashMap;
     pub use crate::helper::helper_fns;
 
     pub fn convert(filename: &String) {
         let mut data = helper_fns::file_into_int_vec(filename);
+        let mut visited: HashMap<i64, i64> = HashMap::new();
+
+        println!("Sorting");
 
         data.sort_by(|a,b| a.partial_cmp(b).unwrap());
         data.insert(0, 0);
+        println!("Sorted");
 
-        let combos = count_paths(&data, 0);
+        let combos = count_paths(&data, 0, visited);
 
         println!("Combos: {}", combos);
     }
 
-    fn count_paths(data: &Vec<i64>, index: i32) -> i32 {
-        let mut paths;
+    fn count_paths(data: &Vec<i64>, index: i64, mut visited: HashMap<i64, i64>) -> i64 {
+        let paths: i64;
         let mut options = 0;
-        if index < (data.len() as i32 - 1) &&
-                data[(index + 1) as usize] - data[index as usize] < 4 {
-            options += count_paths(data, index + 1);
-            if index < (data.len() as i32 - 2) &&
-                    data[(index + 2) as usize] - data[index as usize] < 4 {
-                options += count_paths(data, index + 2);
-                if index < (data.len() as i32 - 3) &&
-                        data[(index + 3) as usize] - data[index as usize] < 4 {
-                    options += count_paths(data, index + 3);
-                }
-            }
-            paths = options;
+        let mut check_dist = 1;
 
+        if visited.get(&index).is_some() {
+            paths = *visited.get(&index).unwrap();
         } else {
-            paths = 1;
+            while index < (data.len() as i64 - check_dist) &&
+                    data[(index + check_dist) as usize] - data[index as usize] < 4 &&
+                    check_dist < 4 {
+                options += count_paths(data, index + check_dist, visited.clone());
+                check_dist +=1;
+            }
+            if check_dist == 1 { options = 1; }
+            paths = options;
+            visited.insert(index, paths);
         }
         paths
     }
  
     pub fn convert_1(filename: &String) {
         let mut data = helper_fns::file_into_int_vec(filename);
-        let mut ones: i32 = 0;
-        let mut threes: i32 = 0;
-        let mut index: i32 = 0;
+        let mut ones: i64 = 0;
+        let mut threes: i64 = 0;
+        let mut index: i64 = 0;
 
         data.sort_by(|a,b| a.partial_cmp(b).unwrap());
 
-        while index < (data.len() - 1) as i32 {
+        while index < (data.len() - 1) as i64 {
             let delta = data[(index + 1) as usize] - data[index as usize];
 
             match delta {
